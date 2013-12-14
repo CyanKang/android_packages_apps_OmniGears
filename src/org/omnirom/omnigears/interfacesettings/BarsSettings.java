95ba9b02531468cbf04849451d45e1e2d2f7b283
Schubi <schubi@erlangen.ccc.de>
2013-12-14 17:23:19 +0100

    Revert "OmniGears: [2/3] Customizable lockscreen & navring targets"
    This reverts commit bc00afebaee96fcaab2ff97cec783e19f335f4ab.

--- src/org/omnirom/omnigears/interfacesettings/BarsSettings.java
+++ src/org/omnirom/omnigears/interfacesettings/BarsSettings.java
@@ -24,7 +24,6 @@ import com.android.settings.R;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.RemoteException;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceGroup;
@@ -32,7 +31,6 @@ import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
-import android.view.WindowManagerGlobal;
 
 public class BarsSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -42,8 +40,6 @@ public class BarsSettings extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
     private static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
     private static final String STATUS_BAR_NETWORK_ACTIVITY = "status_bar_network_activity";
-
-    private static final String CATEGORY_NAVBAR = "category_navigation_bar";
 
     private CheckBoxPreference mStatusBarBrightnessControl;
     private CheckBoxPreference mStatusBarNotifCount;
@@ -86,22 +82,11 @@ public class BarsSettings extends SettingsPreferenceFragment implements
             Settings.System.STATUS_BAR_NETWORK_ACTIVITY, 0) == 1);
         mStatusBarNetworkActivity.setOnPreferenceChangeListener(this);
         mStatusBarNetworkActivity.setOnPreferenceChangeListener(this);
-
-        try {
-            boolean hasNavBar = WindowManagerGlobal.getWindowManagerService().hasNavigationBar();
-            // Hide navigation bar category on devices without navigation bar
-            if (!hasNavBar) {
-                prefSet.removePreference(findPreference(CATEGORY_NAVBAR));
-            }
-        } catch (RemoteException e) {
-            Log.e(TAG, "Error getting navigation bar status");
-        }
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        // If we didn't handle it, let preferences handle it.
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
+        return true;
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -123,6 +108,7 @@ public class BarsSettings extends SettingsPreferenceFragment implements
         } else {
             return false;
         }
+
         return true;
     }
 }
