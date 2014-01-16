1d9abe80683a6799a1b2203cbc760f64669883c9
Schubi <schubi@erlangen.ccc.de>
2014-01-16 17:55:36 +0100

    We have our own disable-camera-setting
    Change-Id: Ibc4b1c8bbb1b27bab88db34f2d5a07cbea474b57

--- src/org/omnirom/omnigears/interfacesettings/LockscreenSettings.java
+++ src/org/omnirom/omnigears/interfacesettings/LockscreenSettings.java
@@ -39,10 +39,8 @@ public class LockscreenSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "LockscreenSettings";
 
-    private static final String KEY_ENABLE_CAMERA = "lockscreen_enable_camera";
     private static final String KEY_SEE_THROUGH = "lockscreen_see_through";
 
-    private CheckBoxPreference mEnableCameraWidget;
     private CheckBoxPreference mSeeThrough;
 
     @Override
@@ -52,50 +50,15 @@ public class LockscreenSettings extends SettingsPreferenceFragment implements
 
         ContentResolver resolver = getActivity().getContentResolver();
 
-        mEnableCameraWidget = (CheckBoxPreference) findPreference(KEY_ENABLE_CAMERA);
-
-        // Enable or disable camera widget settings based on device
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA) ||
-                Camera.getNumberOfCameras() == 0) {
-            mEnableCameraWidget.setEnabled(false);
-        } else if (isCameraDisabledByDpm()) {
-            mEnableCameraWidget.setEnabled(false);
-        } else {
-            mEnableCameraWidget.setChecked(Settings.System.getInt(resolver,
-                    Settings.System.LOCKSCREEN_ENABLE_CAMERA, 0) == 1);
-            mEnableCameraWidget.setOnPreferenceChangeListener(this);
-        }
-
         mSeeThrough = (CheckBoxPreference) findPreference(KEY_SEE_THROUGH);
         mSeeThrough.setChecked(Settings.System.getInt(resolver,
                 Settings.System.LOCKSCREEN_SEE_THROUGH, 1) == 1);
         mSeeThrough.setOnPreferenceChangeListener(this);
     }
 
-    private boolean isCameraDisabledByDpm() {
-        final DevicePolicyManager dpm =
-                (DevicePolicyManager) this.getSystemService(Context.DEVICE_POLICY_SERVICE);
-        if (dpm != null) {
-            try {
-                final int userId = ActivityManagerNative.getDefault().getCurrentUser().id;
-                final int disabledFlags = dpm.getKeyguardDisabledFeatures(null, userId);
-                final  boolean disabledBecauseKeyguardSecure =
-                        (disabledFlags & DevicePolicyManager.KEYGUARD_DISABLE_SECURE_CAMERA) != 0;
-                return dpm.getCameraDisabled(null) || disabledBecauseKeyguardSecure;
-            } catch (RemoteException e) {
-                Log.e(TAG, "Can't get userId", e);
-            }
-        }
-        return false;
-    }
-
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mEnableCameraWidget) {
-            boolean value = (Boolean) objValue;
-            Settings.System.putInt(resolver, Settings.System.LOCKSCREEN_ENABLE_CAMERA, value ? 1 : 0);
-            return true;
-        } else if (preference == mSeeThrough) {
+        if (preference == mSeeThrough) {
             boolean value = (Boolean) objValue;
             Settings.System.putInt(getContentResolver(), Settings.System.LOCKSCREEN_SEE_THROUGH,
                     value ? 1 : 0);
